2258908901c32240bf9d431ae2196bc4e2477abe
Mvge <mvge.dev@gmail.com>
2025-11-10 15:44:12 +0100

    Obstacles
    - Added obstacles object and pairs logic
    - Added spawn logic and movement of obstacles

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,6 +1,20 @@
 extends Node
 
+@onready var player: RigidBody2D = %Player
+@onready var obstacles: Node = $"../Obstacles"
+@onready var border_right: Area2D = $"../Borders/BorderRight"
+
+var OBSTACLES_PAIR = preload("res://scenes/obstacles_pair.tscn")
+# Spawn y -80 to y 80 for obstacles
+
 var game_start = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	spawn_obstacles()
+
+func spawn_obstacles():
+	var obsPair = OBSTACLES_PAIR.instantiate()
+	obstacles.add_child(obsPair)
+	#Define base position
+	obsPair.position.x = border_right.position.x
+	obsPair.position.y = randf_range(-80, 80)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,19 +2,16 @@ extends RigidBody2D
 
 var gravity_affect = false
 var flap_force = -300
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#gravity_scale = 0.0
 	pass
 
-func _process(delta: float) -> void:
-	pass
-		# PAS BON -> Donne de l'impulsion en prenant en compte la force actuelle
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Flap"):
 		print("Flap")
 		linear_velocity = Vector2.ZERO
 		angular_velocity = 0.0
 		apply_central_impulse(Vector2(0, flap_force))
-	
+	 
